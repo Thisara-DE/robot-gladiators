ae179cb329f67f849cb0ec120a210974ec03d967
Glorzo <thisaraeranga@gmail.com>
2022-01-09 19:41:28 -0600

    changed the shop feature to accept 1, 2, 3 as the inputs and pars the inegers to INT

--- assets/js/game.js
+++ assets/js/game.js
@@ -143,27 +143,27 @@ var endGame = function() {
 
 // go to shop between battles function
 var shop = function() {
-  // ask player what they'd like to do
+  // ask player what they'd like to do  
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
-  );
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE.");
 
-  // use switch case to carry out action
-  switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+
+  // use switch case to carry out action  
+  switch (shopOptionPrompt) {    
+    case 1:
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case 2:
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
-      window.alert('Leaving the store.');
-
-      // do nothing, so function will end
+    case 3:
+    case 3:
+      window.alert('Leaving the store.');      
       break;
+      // do nothing, so function will end
     default:
       window.alert('You did not pick a valid option. Try again.');
 
